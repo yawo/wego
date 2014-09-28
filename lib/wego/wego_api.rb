46f3c6a6b1aef71119641f7bfbb6f0ec53f439e8
Guillaume KPOTUFE <mcguy2008@gmail.com>
2014-09-28 23:34:38 +0100

    call api for search and fares

--- lib/wego/wego_api.rb
+++ lib/wego/wego_api.rb
@@ -1,5 +1,6 @@
 require 'restclient'
 require 'restclient/components'
+require 'json'
 require 'rack/cache'
 require 'logger'
 
@@ -28,8 +29,11 @@ module Wego
     # Configure through hash
     def self.configure(opts = {})
       opts.each { |k, v| @@config[k.to_sym] = v if @valid_config_keys.include? k.to_sym }
+      @@config[:searches_url] = @@config[:wego_api_url_prefix]+'/'+@@config[:version]+'/searches'
+      @@config[:fares_url] = @@config[:wego_api_url_prefix]+'/'+@@config[:version]+'/fares'
+      @@config[:currencies_url] = @@config[:wego_api_url_prefix]+'/'+@@config[:version]+'/currencies'
+      @@config[:base_params] = {api_key: @@config[:api_key], ts_code: @@config[:ts_code], locale: @@config[:locale]}
     end
-
     # Configure through yaml file
     def self.configure_with(path_to_yaml_file)
       begin
@@ -42,11 +46,10 @@ module Wego
       end
     end
 
+    #Configure defaults
+    self.configure
+
     def self.config
-      @@config[:searches_url] = @@config[:wego_api_url_prefix]+'/'+@@config[:version]+'/searches'
-      @@config[:fares_url] = @@config[:wego_api_url_prefix]+'/'+@@config[:version]+'/fares'
-      @@config[:currencies_url] = @@config[:wego_api_url_prefix]+'/'+@@config[:version]+'/currencies'
-      @@config[:base_params] = {api_key: @@config[:api_key], ts_code: @@config[:ts_code], locale: @@config[:locale]}
       @@config
     end
 
@@ -58,20 +61,37 @@ module Wego
       puts "Wego api Initialized"
     end
 
-    def search_trip(departure_code, arrival_code, outbound_date, inbound_date, adults_count)
-      trips = []
-      trips.push(
+    def search_trips(departure_code, arrival_code, outbound_date, inbound_date, adults_count)
+      trips = [
           {
               departure_code: departure_code,
               arrival_code: arrival_code,
               outbound_date: outbound_date,
               inbound_date: inbound_date
           }
-      )
+      ]
       postBody = {trips: trips, departure_city: true, arrival_city: true, adults_count: adults_count}
-      response = RestClient.post(@@config[:searches_url], postBody, params: @@config[:base_params])
-      @@logger.debug(" response is ::: #{response.dump}")
+      tripResponse = postToWego(@@config[:searches_url],postBody)
+      if(tripResponse.code == 200)
+        sleep(10) #Or do something during that 10 sec.
+        get_fares(tripResponse.id,tripResponse.trips[0].id)
+      end
+    end
 
+    def get_fares(search_id, trip_id, filters = {}  )
+      postBody = {
+          id:               Random.new_seed,
+          search_id:        search_id,
+          trip_id:          trip_id,
+          fares_query_type: 'route'
+      }.merge(filters)
+      postToWego(@@config[:fares_url],postBody)
+    end
+
+    def postToWego(url,postBody)
+      response = RestClient.post(url, postBody.to_json, {params: @@config[:base_params]}.merge(@@config[:base_headers]))
+      @@logger.debug(" response is :::\n #{response.dump}\n------------")
+      response
     end
 
   end
